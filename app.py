01b3b43531d40a3eecd2b7daaf59b37c872f3370
alexchristie12 <alex.christie@my.jcu.edu.au>
2023-11-26 12:10:16 +1000

    Complete Flask exercise

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World :)<h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def convert_celsius_to_fahrenheit(celsius=""):
+    celsius = float(celsius)
+    return celsius_to_fahrenheit(celsius)
+
+
+def celsius_to_fahrenheit(celsius):
+    """Converts Celsius to Fahrenheit"""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return str(fahrenheit)
+
+
+if __name__ == '__main__':
+    app.run()
